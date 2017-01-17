c1754851f629137f117e49cc0b745f7c055f67b4
efueger <efueger@users.noreply.github.com>
2017-01-17 12:25:09 -0500

    Create application.js

---
+++ app/assets/application.js
@@ -0,0 +1,18 @@
+var defaultFrom = mymoments().subtract(30, 'days').format('L'),
+        defaultTo = mymoments().format('L');
+    // initialize bootstrap date picker and set the to and from dates
+    $('.fromDate').datepicker({
+      todayBtn: "linked",
+      autoclose: true,
+      todayHighlight: true
+    }).datepicker('update', defaultFrom);
+
+    $('.toDate').datepicker({
+      todayBtn: "linked",
+      autoclose: true,
+      todayHighlight: true
+    }).datepicker('update', defaultTo);
+
+    // initialize bootstrap tooltip
+    $('[data-toggle="tooltip"]').tooltip({'trigger': 'click hover'});
+});
